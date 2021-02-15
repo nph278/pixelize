c9eb04abb810294f9d848a0f4f246d74c1ecbb9e
nph <carllegrone@gmail.com>
2021-02-15 14:39:01 -0500

    animated bullets (test)

--- test/src/sprites.js
+++ test/src/sprites.js
@@ -15,9 +15,11 @@ export const shipDown = animation(
 export const box = animation(
   "#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade.......#ce2ade.#ce2ade.......#ce2ade.#ce2ade.......#ce2ade.#ce2ade.......#ce2ade.#ce2ade.......#ce2ade.#ce2ade.......#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade.#ce2ade"
 );
-export const bulletVertical = animation(
-  ".........................#dea700.#ded000.#dea700.#ded000.#dea700.#ded000...#ded000.#dea700.#ded000.#dea700.#ded000.#dea700........................."
-);
-export const bulletHorizontal = animation(
-  "...........#ded000.#dea700.......#dea700.#ded000.......#ded000.#dea700.......#dea700.#ded000.......#ded000.#dea700.......#dea700.#ded000..........."
-);
+export const bulletVertical = animation([
+  ".........................#dea700.#ded000.#dea700.#ded000.#dea700.#ded000...#ded000.#dea700.#ded000.#dea700.#ded000.#dea700.........................",
+  ".........................#ded000.#dea700.#ded000.#dea700.#ded000.#dea700...#dea700.#ded000.#dea700.#ded000.#dea700.#ded000.........................",
+]);
+export const bulletHorizontal = animation([
+  "...........#ded000.#dea700.......#dea700.#ded000.......#ded000.#dea700.......#dea700.#ded000.......#ded000.#dea700.......#dea700.#ded000...........",
+  "...........#dea700.#ded000.......#ded000.#dea700.......#dea700.#ded000.......#ded000.#dea700.......#dea700.#ded000.......#ded000.#dea700...........",
+]);
