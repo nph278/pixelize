be5983497c61366b8171c15e6e3b2dfcfa04e3c2
nph <carllegrone@gmail.com>
2021-02-16 19:33:27 -0500

    minify site

--- site/.eleventy.js
+++ site/.eleventy.js
@@ -1,11 +1,27 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.markdownTemplateEngine = "nkj";
+
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+        minifyCSS: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     markdownTemplateEngine: "njk",
